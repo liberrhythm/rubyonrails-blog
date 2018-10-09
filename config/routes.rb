660493c1bd5c7e5260035bdf382e8d841e4072aa
Sabrina Peng <sabrina.h.peng@gmail.com>
2018-10-09 15:37:21 -0500

    remove welcome controller and files, root routes to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
+  get 'articles/index'
+  root 'articles#index'
   resources :articles do
     resources :comments # nested resource
   end
